9e60dfe6765051235ebd4b1f573ea89194357c57
Mohit <s.mohit3547@gmail.com>
2026-04-02 16:12:55 +0530

    ff

--- app.js
+++ app.js
@@ -1,35 +1,25 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-// import cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
+import "./config/database.js"
+import User from './model/userSchema.js';
+import Song from './model/songSchema.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 const app = express();
-
-app.use(cookieParser()) 
 
 app.use(cors({
   origin: process.env.ORIGIN || "http://localhost:5173",
   credentials: true
 }));
 app.use(express.json());
+app.use(cookieParser());  // ← ye add karo
 
+app.use("/auth", userRoutes);
+app.use("/api", userRoutes);
 
-// connect to database
-import "./config/database.js"
-
-// import moodels
-import User from './model/userSchema.js';
-import Song from './model/songSchema.js';
-
-// import routes
-import userRoutes from './routes/userRoutes.js';
-import cookieParser from 'cookie-parser';
-
-app.use("/auth",userRoutes)
-app.use("/api",userRoutes)
-// console.log(process.env.MONGO_DB)
-
-app.listen("5010",() => {
+app.listen("5010", () => {
     console.log("Server is running on port 5010");
-})
+});
