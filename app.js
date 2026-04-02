159d6519abc397a4cd9f36cacbb44fd9cf10752c
Mohit <s.mohit3547@gmail.com>
2026-04-02 17:35:03 +0530

    cors updated

--- app.js
+++ app.js
@@ -14,9 +14,21 @@ const allowedOrigins = ["http://localhost:5173", process.env.ORIGIN];
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        "http://localhost:5173",
+        "https://x-tune-application.vercel.app"
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-  }),
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
 );
 app.use(express.json());
 app.use(cookieParser()); // ← ye add karo
