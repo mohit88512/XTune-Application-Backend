20fdfa4b8763375fad8c1f8cf820e7fe85d61e97
Mohit <s.mohit3547@gmail.com>
2026-04-02 16:46:20 +0530

    upadte app.js

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import "./config/database.js"
-import User from './model/userSchema.js';
-import Song from './model/songSchema.js';
-import userRoutes from './routes/userRoutes.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import "./config/database.js";
+import User from "./model/userSchema.js";
+import Song from "./model/songSchema.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 const app = express();
 
-app.use(cors({
-  origin: process.env.ORIGIN || "http://localhost:5173",
-  credentials: true
-}));
+const allowedOrigins = ["http://localhost:5173", process.env.ORIGIN];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  }),
+);
 app.use(express.json());
-app.use(cookieParser());  // ← ye add karo
+app.use(cookieParser()); // ← ye add karo
 
 app.use("/auth", userRoutes);
 app.use("/api", userRoutes);
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
 
-app.listen("5010", () => {
-    console.log("Server is running on port 5010");
+const PORT = process.env.PORT || 5010;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
